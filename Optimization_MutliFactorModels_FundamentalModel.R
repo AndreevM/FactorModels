708757f39403a25f538c4c199f9f5ec2bc7eb7d0
quant-trading <qnt.trading@gmail.com>
2017-08-07 20:38:23 -0700

    Файлы к статье

---
+++ Optimization_MutliFactorModels_FundamentalModel.R
@@ -0,0 +1,154 @@
+
+# Устанавливаем и загружаем пакет PortfolioAnalytics
+#install.packages("PortfolioAnalytics")
+library(PortfolioAnalytics)
+
+# Устанавливаем и загружаем пакет DEoptim
+#install.packages("DEoptim")
+library(DEoptim)
+
+# устанавливаем и загружаем пакет FactorAnalytics
+#install.packages("githubinstall")
+#install.packages("pcaPP")
+#library(githubinstall)
+#githubinstall("R-Finance/FactorAnalytics")
+library(factorAnalytics)
+
+# Устанавливаем и загружаем пакет quantmod
+#install.packages("quantmod")
+library(quantmod)
+
+# очистка среды 
+rm(list=ls())
+
+# устанавливаем random.seed для воспроизводимости результатов
+set.seed(1234)
+
+# загружаем состав индекса ММВБ
+benchmark <- read.csv(file = "micex_ind_structure.csv", header = T, stringsAsFactors = F, sep = ";")
+
+# выводим первые несколько записей
+head(benchmark)
+
+# загружаем котировки из csv и преобразуем их в объект xts (eXtended Time Series)
+returns <- read.csv(file = "quotes_indx.csv", header = T, stringsAsFactors = F, sep = ",")
+returns$Date <- as.Date(returns$Date)
+returns <- xts(returns[,-1], order.by=returns[,1])
+
+# Вычисляем дневные log-доходности на основе цен с помощью функции ROC
+returns <- ROC(returns, type = "discrete")
+returns <- returns[-1,]
+
+# создаем целевой портфель и ограничения
+
+# создаем объект типа портфель и передаем в него множество акций, в которые мы будем инвестировать, и задаем сетку поиска
+portfolio.spec <- portfolio.spec(assets=benchmark$ticker, weight_seq = generatesequence(min=1e-5, max=0.15, by=0.0001, rounding = 4))
+
+# ограничение: сумма весов всех активов в портфеле равна 1, использование заемных средств запрещено
+portfolio.spec <- add.constraint(portfolio=portfolio.spec, type="weight_sum", min_sum=0.99, max_sum=1.01)
+
+# ограничение: кол-во различных инструментов в портфеле не превышает 15
+portfolio.spec <- add.constraint(portfolio=portfolio.spec, type="position_limit", max_pos=15)
+
+# ограничение: доля отдельной акции в портфеле не превышает 15%, короткие продажи запрещены
+portfolio.spec <- add.constraint(portfolio=portfolio.spec, type="box", min=0.00, max=0.15)
+
+# добавлям запрет на инвестиции в акции химической отрасли (CHEMICAL)
+portfolio.spec <- add.constraint(portfolio=portfolio.spec, type="group",
+                                 groups=list(groupRestricted=which(benchmark$industry == "CHEMICAL"),
+                                             grouAllowed=which(benchmark$industry != "CHEMICAL")),
+                                 group_min=c(0.0, 0.99),
+                                 group_max=c(0.0, 1.01))
+
+# добавляем штрафную функцию в виде транзакционных расходов 0.20%
+portfolio.spec <- add.constraint(portfolio=portfolio.spec, type="transaction_cost", ptc=0.002)
+
+
+
+# создаем целевую функцию минимизации активной вариации портфеля
+activeRiskObj = function(weights, sigma =0 , b.weights=0)
+{
+  # вычисляем активные веса тестового портфеля
+  active.weights = weights - b.weights
+
+  # вычисляем активный риск и возвращаем его значение  
+  active.var = t(active.weights) %*% sigma %*% active.weights
+  print(sqrt(active.var)*sqrt(252)*10000)
+  return(active.var)
+}
+
+# задаем фундаментальную модель
+sigma.model <- function(R){
+
+  stock.m <- read.csv(file = "industry_factors.csv", header = T, stringsAsFactors = F, sep = ",")
+
+  # stock.m[c(1,730,3315, 3702),]
+
+  exposure.names <- c("OIL","ENERGY","FINANCE","RETAIL", "OTHER")
+
+  # строим многофакторную фундаментальную модель
+  fundamental.fit <- fitFundamentalFactorModel(data=stock.m,
+                                               exposure.names=exposure.names,
+                                               datevar = "DATE",
+                                               returnsvar = "RETURN",
+                                               assetvar = "TICKER", wls = T,
+                                               regression = "classic",
+                                               covariance = "classic", full.resid.cov = F,
+                                               robust.scale = F)
+  # извлекаем ковариационную матрицу из модели
+  sigma <- fundamental.fit$returns.cov$cov
+  # возвращаем результат
+  out <- list()
+  out$sigma <- sigma
+  return(out)
+}
+
+
+# задаем целевую функцию
+portfolio.spec <- add.objective(portfolio=portfolio.spec, type="risk", name="activeRiskObj",
+                            arguments=list(b.weights = benchmark$weight))
+
+# запускаем алгоритм оптимизации портфеля
+.storage <<- new.env()
+opt.pasd <- optimize.portfolio(R = returns['2017-02-11/'], portfolio = portfolio.spec,
+                               optimize_method="DEoptim",
+                               momentFUN = "sigma.model",
+                               search_size = 200)
+
+# выводим результаты оптимизации
+print(sqrt(opt.pasd$objective_measures$activeRiskObj * 252) * 10000)
+
+optimal.weights <- opt.pasd$weights
+names(optimal.weights) <- benchmark$ticker
+barplot(optimal.weights[optimal.weights > 0])
+
+
+
+# Мультифакторная фундаментальная модель
+stock.m <- read.csv(file = "industry_factors.csv", header = T, stringsAsFactors = F, sep = ",")
+
+stock.m[c(1,730,3315, 3702),]
+
+exposure.names <- c("OIL","ENERGY","FINANCE","RETAIL", "OTHER")
+
+fundamental.fit <- fitFundamentalFactorModel(data=stock.m,
+                                              exposure.names=exposure.names,
+                                              datevar = "DATE", 
+                                              returnsvar = "RETURN",
+                                              assetvar = "TICKER", wls = T,
+                                              regression = "classic",
+                                              covariance = "classic", full.resid.cov = F,
+                                              robust.scale = F)
+
+fundamental.fit$resids.cov
+fundamental.fit$factor.cov$cov
+fundamental.fit$tstats
+fundamental.fit$returns.cov$cov
+
+
+# выводим на график доходности риск-факторов
+library(PerformanceAnalytics)
+chart.CumReturns(fundamental.fit$factor.returns, main = "Кумулятивные доходности риск-факторов", legend.loc="topleft")
+
+
+
---
+++ Optimization_MutliFactorModels_MacroModel.R
@@ -0,0 +1,122 @@
+
+# Устанавливаем и загружаем пакет PortfolioAnalytics
+#install.packages("PortfolioAnalytics")
+library(PortfolioAnalytics)
+
+# Устанавливаем и загружаем пакет DEoptim
+#install.packages("DEoptim")
+library(DEoptim)
+
+# устанавливаем и загружаем пакет FactorAnalytics
+#install.packages("githubinstall")
+#install.packages("pcaPP")
+#library(githubinstall)
+#githubinstall("R-Finance/FactorAnalytics")
+library(factorAnalytics)
+
+# Устанавливаем и загружаем пакет quantmod
+#install.packages("quantmod")
+library(quantmod)
+
+# очистка среды 
+rm(list=ls())
+
+# устанавливаем random.seed для воспроизводимости результатов
+set.seed(1234)
+
+# загружаем состав индекса ММВБ
+benchmark <- read.csv(file = "micex_ind_structure.csv", header = T, stringsAsFactors = F, sep = ";")
+
+# выводим первые несколько записей
+head(benchmark)
+
+# загружаем котировки из csv и преобразуем их в объект xts (eXtended Time Series)
+returns <- read.csv(file = "quotes_indx.csv", header = T, stringsAsFactors = F, sep = ",")
+returns$Date <- as.Date(returns$Date)
+returns <- xts(returns[,-1], order.by=returns[,1])
+
+# Вычисляем дневные log-доходности на основе цен с помощью функции ROC
+returns <- ROC(returns, type = "discrete")
+returns <- returns[-1,]
+
+# создаем целевой портфель и ограничения
+
+# создаем объект типа портфель и передаем в него множество акций, в которые мы будем инвестировать, и задаем сетку поиска
+portfolio.spec <- portfolio.spec(assets=benchmark$ticker, weight_seq = generatesequence(min=1e-5, max=0.15, by=0.0001, rounding = 4))
+
+# ограничение: сумма весов всех активов в портфеле равна 1, использование заемных средств запрещено
+portfolio.spec <- add.constraint(portfolio=portfolio.spec, type="weight_sum", min_sum=0.99, max_sum=1.01)
+
+# ограничение: кол-во различных инструментов в портфеле не превышает 15
+portfolio.spec <- add.constraint(portfolio=portfolio.spec, type="position_limit", max_pos=15)
+
+# ограничение: доля отдельной акции в портфеле не превышает 15%, короткие продажи запрещены
+portfolio.spec <- add.constraint(portfolio=portfolio.spec, type="box", min=0.00, max=0.15)
+
+# добавлям запрет на инвестиции в акции химической отрасли (CHEMICAL)
+portfolio.spec <- add.constraint(portfolio=portfolio.spec, type="group",
+                                 groups=list(groupRestricted=which(benchmark$industry == "CHEMICAL"),
+                                             grouAllowed=which(benchmark$industry != "CHEMICAL")),
+                                 group_min=c(0.0, 0.99),
+                                 group_max=c(0.0, 1.01))
+
+# добавляем штрафную функцию в виде транзакционных расходов 0.20%
+portfolio.spec <- add.constraint(portfolio=portfolio.spec, type="transaction_cost", ptc=0.002)
+
+
+
+# создаем целевую функцию минимизации активной вариации портфеля
+activeRiskObj = function(weights, sigma =0 , b.weights=0)
+{
+  # вычисляем активные веса тестового портфеля
+  active.weights = weights - b.weights
+
+  # вычисляем активный риск и возвращаем его значение  
+  active.var = t(active.weights) %*% sigma %*% active.weights
+  print(sqrt(active.var)*sqrt(252)*10000)
+  return(active.var)
+}
+
+
+# строим многофакторную макроэкономическую модель
+sigma.model <- function(R){
+  
+  macro.ret <- read.csv(file = "macro_returns.csv", header = T, stringsAsFactors = F, sep = ",")
+  macro.ret$Date <- as.Date(macro.ret$Date)
+  macro.ret <-  xts(macro.ret[,-1], order.by=macro.ret[,1])
+  head(macro.ret)
+
+  time.series <- merge.xts(returns, macro.ret)
+
+  macro.fit <- fitTimeSeriesFactorModel(assets.names=colnames(time.series),
+                                        factors.names=c("BRENT","USD.RUB","COMEX.GC"),
+                                        data=time.series,fit.method="DLS", decay.factor = 0.81)
+
+  
+  N = 50
+  sigma <- factorModelCovariance(macro.fit$beta[seq(1:N),],var(macro.ret),macro.fit$resid.variance[seq(1:N)])
+
+  # возвращаем результат
+  out <- list()
+  out$sigma <- sigma
+  return(out)
+}
+
+
+# задаем целевую функцию
+portfolio.spec <- add.objective(portfolio=portfolio.spec, type="risk", name="activeRiskObj",
+                            arguments=list(b.weights = benchmark$weight))
+
+# запускаем алгоритм оптимизации портфеля
+.storage <<- new.env()
+opt.pasd <- optimize.portfolio(R = returns['2017-02-11/'], portfolio = portfolio.spec,
+                               optimize_method="DEoptim",
+                               momentFUN = "sigma.model",
+                               search_size = 200)
+
+# выводим результаты оптимизации
+print(sqrt(opt.pasd$objective_measures$activeRiskObj * 252) * 10000)
+
+optimal.weights <- opt.pasd$weights
+names(optimal.weights) <- benchmark$ticker
+barplot(optimal.weights[optimal.weights > 0])
